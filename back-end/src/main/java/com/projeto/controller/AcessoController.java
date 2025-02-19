3d3b69597fbbe8ce21beac60951bc21be47e5d4a
eduardo <edu.carvc@gmail.com>
2025-02-19 19:01:46 -0300

    Correção Spring

--- back-end/src/main/java/com/projeto/controller/AcessoController.java
+++ back-end/src/main/java/com/projeto/controller/AcessoController.java
@@ -1,5 +1,8 @@
 package com.projeto.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/acessos")
 public class AcessoController {}
